91dcef7de4dde410cd0f302861866f24e666976c
Vinod Kumar Sondhiya <vsondhiya@gmail.com>
2025-08-25 00:00:49 +0530

    rg19 added

--- main.tf
+++ main.tf
@@ -15,3 +15,8 @@ resource "azurerm_resource_group" "rg_block18" {
   name     = "vinod_rg18"
   location = "West Europe"
 }
+
+resource "azurerm_resource_group" "rg_block19" {
+  name     = "vinod_rg19"
+  location = "West Europe"
+}
