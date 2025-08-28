5210d15cdca8291fdbec6080e5c639b4ed61d663
Vsondhiya <vsondhiya@gmail.com>
2025-08-28 11:33:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,6 @@ resource "azurerm_resource_group" "rg_block99" {
   name     = "vinod_rg99"
   location = "West US"
 }
-feature/04
 resource "azurerm_resource_group" "rg_block100" {
   name     = "vinod_rg100"
   location = "West US"
